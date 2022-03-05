c409df3c932218b450f59fc605f279300af40c40
nputrautama <nyoman.putra@ruangguru.com>
2022-03-05 15:23:06 +0800

    feat: add example moor

---
+++ lib/local_database/database.g.dart
@@ -0,0 +1,228 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'database.dart';
+
+// **************************************************************************
+// MoorGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps, unnecessary_this
+class TodoData extends DataClass implements Insertable<TodoData> {
+  final int? id;
+  final String title;
+  final String description;
+  TodoData({this.id, required this.title, required this.description});
+  factory TodoData.fromData(Map<String, dynamic> data, GeneratedDatabase db,
+      {String? prefix}) {
+    final effectivePrefix = prefix ?? '';
+    return TodoData(
+      id: const IntType().mapFromDatabaseResponse(data['${effectivePrefix}id']),
+      title: const StringType()
+          .mapFromDatabaseResponse(data['${effectivePrefix}title'])!,
+      description: const StringType()
+          .mapFromDatabaseResponse(data['${effectivePrefix}description'])!,
+    );
+  }
+  @override
+  Map<String, Expression> toColumns(bool nullToAbsent) {
+    final map = <String, Expression>{};
+    if (!nullToAbsent || id != null) {
+      map['id'] = Variable<int?>(id);
+    }
+    map['title'] = Variable<String>(title);
+    map['description'] = Variable<String>(description);
+    return map;
+  }
+
+  TodoCompanion toCompanion(bool nullToAbsent) {
+    return TodoCompanion(
+      id: id == null && nullToAbsent ? const Value.absent() : Value(id),
+      title: Value(title),
+      description: Value(description),
+    );
+  }
+
+  factory TodoData.fromJson(Map<String, dynamic> json,
+      {ValueSerializer? serializer}) {
+    serializer ??= moorRuntimeOptions.defaultSerializer;
+    return TodoData(
+      id: serializer.fromJson<int?>(json['id']),
+      title: serializer.fromJson<String>(json['title']),
+      description: serializer.fromJson<String>(json['description']),
+    );
+  }
+  @override
+  Map<String, dynamic> toJson({ValueSerializer? serializer}) {
+    serializer ??= moorRuntimeOptions.defaultSerializer;
+    return <String, dynamic>{
+      'id': serializer.toJson<int?>(id),
+      'title': serializer.toJson<String>(title),
+      'description': serializer.toJson<String>(description),
+    };
+  }
+
+  TodoData copyWith({int? id, String? title, String? description}) => TodoData(
+        id: id ?? this.id,
+        title: title ?? this.title,
+        description: description ?? this.description,
+      );
+  @override
+  String toString() {
+    return (StringBuffer('TodoData(')
+          ..write('id: $id, ')
+          ..write('title: $title, ')
+          ..write('description: $description')
+          ..write(')'))
+        .toString();
+  }
+
+  @override
+  int get hashCode => Object.hash(id, title, description);
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is TodoData &&
+          other.id == this.id &&
+          other.title == this.title &&
+          other.description == this.description);
+}
+
+class TodoCompanion extends UpdateCompanion<TodoData> {
+  final Value<int?> id;
+  final Value<String> title;
+  final Value<String> description;
+  const TodoCompanion({
+    this.id = const Value.absent(),
+    this.title = const Value.absent(),
+    this.description = const Value.absent(),
+  });
+  TodoCompanion.insert({
+    this.id = const Value.absent(),
+    required String title,
+    required String description,
+  })  : title = Value(title),
+        description = Value(description);
+  static Insertable<TodoData> custom({
+    Expression<int?>? id,
+    Expression<String>? title,
+    Expression<String>? description,
+  }) {
+    return RawValuesInsertable({
+      if (id != null) 'id': id,
+      if (title != null) 'title': title,
+      if (description != null) 'description': description,
+    });
+  }
+
+  TodoCompanion copyWith(
+      {Value<int?>? id, Value<String>? title, Value<String>? description}) {
+    return TodoCompanion(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+    );
+  }
+
+  @override
+  Map<String, Expression> toColumns(bool nullToAbsent) {
+    final map = <String, Expression>{};
+    if (id.present) {
+      map['id'] = Variable<int?>(id.value);
+    }
+    if (title.present) {
+      map['title'] = Variable<String>(title.value);
+    }
+    if (description.present) {
+      map['description'] = Variable<String>(description.value);
+    }
+    return map;
+  }
+
+  @override
+  String toString() {
+    return (StringBuffer('TodoCompanion(')
+          ..write('id: $id, ')
+          ..write('title: $title, ')
+          ..write('description: $description')
+          ..write(')'))
+        .toString();
+  }
+}
+
+class $TodoTable extends Todo with TableInfo<$TodoTable, TodoData> {
+  @override
+  final GeneratedDatabase attachedDatabase;
+  final String? _alias;
+  $TodoTable(this.attachedDatabase, [this._alias]);
+  final VerificationMeta _idMeta = const VerificationMeta('id');
+  @override
+  late final GeneratedColumn<int?> id = GeneratedColumn<int?>(
+      'id', aliasedName, true,
+      type: const IntType(),
+      requiredDuringInsert: false,
+      defaultConstraints: 'PRIMARY KEY AUTOINCREMENT');
+  final VerificationMeta _titleMeta = const VerificationMeta('title');
+  @override
+  late final GeneratedColumn<String?> title = GeneratedColumn<String?>(
+      'title', aliasedName, false,
+      type: const StringType(), requiredDuringInsert: true);
+  final VerificationMeta _descriptionMeta =
+      const VerificationMeta('description');
+  @override
+  late final GeneratedColumn<String?> description = GeneratedColumn<String?>(
+      'description', aliasedName, false,
+      type: const StringType(), requiredDuringInsert: true);
+  @override
+  List<GeneratedColumn> get $columns => [id, title, description];
+  @override
+  String get aliasedName => _alias ?? 'todo';
+  @override
+  String get actualTableName => 'todo';
+  @override
+  VerificationContext validateIntegrity(Insertable<TodoData> instance,
+      {bool isInserting = false}) {
+    final context = VerificationContext();
+    final data = instance.toColumns(true);
+    if (data.containsKey('id')) {
+      context.handle(_idMeta, id.isAcceptableOrUnknown(data['id']!, _idMeta));
+    }
+    if (data.containsKey('title')) {
+      context.handle(
+          _titleMeta, title.isAcceptableOrUnknown(data['title']!, _titleMeta));
+    } else if (isInserting) {
+      context.missing(_titleMeta);
+    }
+    if (data.containsKey('description')) {
+      context.handle(
+          _descriptionMeta,
+          description.isAcceptableOrUnknown(
+              data['description']!, _descriptionMeta));
+    } else if (isInserting) {
+      context.missing(_descriptionMeta);
+    }
+    return context;
+  }
+
+  @override
+  Set<GeneratedColumn> get $primaryKey => {id};
+  @override
+  TodoData map(Map<String, dynamic> data, {String? tablePrefix}) {
+    return TodoData.fromData(data, attachedDatabase,
+        prefix: tablePrefix != null ? '$tablePrefix.' : null);
+  }
+
+  @override
+  $TodoTable createAlias(String alias) {
+    return $TodoTable(attachedDatabase, alias);
+  }
+}
+
+abstract class _$AppDatabase extends GeneratedDatabase {
+  _$AppDatabase(QueryExecutor e) : super(SqlTypeSystem.defaultInstance, e);
+  late final $TodoTable todo = $TodoTable(this);
+  late final TodoDao todoDao = TodoDao(this as AppDatabase);
+  @override
+  Iterable<TableInfo> get allTables => allSchemaEntities.whereType<TableInfo>();
+  @override
+  List<DatabaseSchemaEntity> get allSchemaEntities => [todo];
+}
