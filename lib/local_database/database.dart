27ba0025754cf6ac7d266f39b8a8cd64e3cbac33
nputrautama <nyoman.putra@ruangguru.com>
2022-03-26 23:08:03 +0800

    feat: implement object box

--- lib/local_database/database.dart
+++ lib/local_database/database.dart
@@ -3,8 +3,5 @@ import 'package:todo_list_app/objectbox.g.dart';
 class Database{
   Database();
 
-  Future<Store> getInstance() async{
-    Store store = await openStore();
-    return store;
-  }
+  Future<Store> getInstance() async => await openStore();
 }
